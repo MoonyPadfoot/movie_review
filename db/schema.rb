a5a25e1a191fce0d6c37e5dfd702201f4b6d7307
Renier Antonio <eurekavixen@gmail.com>
2024-10-11 15:45:37 +0800

    added: genre model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_11_072918) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_11_074444) do
+  create_table "genres", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "blurb"
