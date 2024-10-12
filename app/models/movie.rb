92bed718f3a7a1fc7acab072dfb6a559aa0a7baa
Renier Antonio <eurekavixen@gmail.com>
2024-10-12 12:05:41 +0800

    added: released date comparison validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :blurb, presence: true, length: { minimum: 20 }
-  validates :date_released, presence: true
+  validates :date_released, presence: true, comparison: { less_than_or_equal_to: :showing_start }
   validates :country_of_origin, presence: true, length: { minimum: 2 }
   validates :showing_start, presence: true, comparison: { less_than: :showing_end }
   validates :showing_end, presence: true, comparison: { greater_than: :showing_start }
