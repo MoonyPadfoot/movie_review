f2ec6e92fb764eb7ad94605f6cc52d323867e4ff
Renier Antonio <eurekavixen@gmail.com>
2024-10-12 12:06:26 +0800

    added: create and edit action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,20 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      flash[:notice] = 'Movie created successfully!'
+      redirect_to movies_path
+    else
+      flash.now[:alert] = 'Movie creation failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
   def movie_params
     params.require(:movie).permit(:title, :blurb, :date_released, :country_of_origin, :showing_start, :showing_end)
   end
