8602bc69565eef0ec5a8d325a415efe9782c248f
Renier Antonio <eurekavixen@gmail.com>
2024-10-14 13:10:38 +0800

    added: validations to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
+  validates :content, presence: true, length: { minimum: 20 }
+  validates :rating, presence: true, numericality: { only_integer: true }
+
   belongs_to :user
   belongs_to :movie
 end
