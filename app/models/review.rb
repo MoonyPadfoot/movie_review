cc687ca34e8ece93366bc3633e58dad7e7ee1d8a
Renier Antonio <eurekavixen@gmail.com>
2024-10-14 14:38:23 +0800

    modified: inclusion validation for rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   validates :content, presence: true, length: { minimum: 20 }
-  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: 0..5
 
   belongs_to :user
   belongs_to :movie
