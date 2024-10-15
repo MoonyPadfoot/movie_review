0a19d82eb2dd7ecde319980eb1232d62b6588fc1
Renier Antonio <eurekavixen@gmail.com>
2024-10-15 10:23:32 +0800

    modified: adjusted allowed rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   validates :content, presence: true, length: { minimum: 20 }
-  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: 0..5
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: 1..5
 
   belongs_to :user
   belongs_to :movie
