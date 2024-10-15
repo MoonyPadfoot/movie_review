9f9c46ba713257b4c710b627a872af41a78b6ff1
Renier Antonio <eurekavixen@gmail.com>
2024-10-15 15:00:58 +0800

    modified: removed email requirement on user creation

---
+++ db/migrate/20241015065224_update_email_column_nullable.rb
@@ -0,0 +1,5 @@
+class UpdateEmailColumnNullable < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :users, :email, :string, null: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_14_062046) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_15_065915) do
   create_table "genres", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_14_062046) do
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -59,7 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_14_062046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
