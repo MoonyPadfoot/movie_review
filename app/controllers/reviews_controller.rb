d61667152fa8fe87242d0eff0d8d526aa4b14784
Renier Antonio <eurekavixen@gmail.com>
2024-10-15 14:40:31 +0800

    added: editable user comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,7 +22,7 @@ class ReviewsController < ApplicationController
   def update
     if @review.update(review_params)
       flash[:notice] = 'Comment updated successfully'
-      redirect_to movie_reviews_path(@movie)
+      redirect_to movie_path(@movie)
     else
       render :edit
     end
@@ -37,7 +37,7 @@ class ReviewsController < ApplicationController
   private
 
   def set_movie
-    @movie = Movie.find 1
+    @movie = Movie.find(params[:movie_id])
   end
 
   def set_review
